1bcac2cc54eebcdcaa8aefc235a0728beb6ecade
frentester <37140983+frentester@users.noreply.github.com>
2019-05-22 16:36:15 +0800

    Add files via upload

---
+++ ALU.v
@@ -0,0 +1,21 @@
+module ALU (A, B, SEL, OUT);
+	
+	input [3:0] A, B;
+	input [2:0] SEL;
+  output [7:0] OUT;
+  
+  reg [7:0] OUT;
+  
+  always @(A or B or SEL)
+    case(SEL)
+      3'b000: OUT = A + B;
+      3'b001: OUT = A - B;
+      3'b010: OUT = A * B;
+      3'b011: OUT = A / B;
+      3'b100: OUT = A & B;
+      3'b101: OUT = A | B;
+      3'b110: OUT = A ^ B;
+      3'b111: OUT = ~A;
+    endcase
+
+endmodule
